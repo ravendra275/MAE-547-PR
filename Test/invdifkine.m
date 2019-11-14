e5b0f53ecb67a237ef8b9fa14abdc45f702fb44d
Ravendra <ravendra275@users.noreply.github.com>
2019-11-14 12:37:58 -0700

    New type

--- Test/invdifkine.m
+++ Test/invdifkine.m
@@ -1,20 +1,17 @@
 clc;
 clear all;
-
 syms t
+double(t)
 pd = [0.42*cos(t *pi/10); 0.42*sin(t*pi/10); 0.1*(1+sin(t))];
-pd_dot = diff(pd)
-sym2poly(t);
+pd_dot = diff(pd);
 t = 1:.1:2;
-pd
+pd 
 pd_dot
 % q1=zeros(length(pd),length(t));
 % q2=zeros(length(pd_dot),length(t));
-% for t = 1:.1:2;
-%    
+% for t = 1:.1:2;   
 %         q1(t,:) = pd;
 %         q2(t,:) = pd_dot;
-%    
 % end
 % q1
 % q2

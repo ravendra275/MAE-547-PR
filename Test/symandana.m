cd9de88ec75c4cada8ed666bae6f4e4d89d6bacb
MalayNagda <35292631+MalayNagda@users.noreply.github.com>
2019-11-13 17:28:31 -0700

    Create symandana.m

---
+++ Test/symandana.m
@@ -0,0 +1,225 @@
+clc
+clear all
+close all
+format compact
+sympref('FloatingPointOutput',true)
+
+x= input('Enter number of links: ');
+for i=1:x
+    if i==1
+        fprintf('Press p for prismatic joint,\nPress r for revolute joint\n');
+    end
+    fprintf('Is Link %d',i)
+    por(i)=input(' prismatic or revolute: ','s');
+    if xor(strcmp(por(i),'r'),strcmp(por(i),'p')) == 0
+        fprintf('Invalid input. Press p for prismatic joint, Press r for revolute joint\n');
+        %exit;
+    end
+end
+
+syms q [1 x];
+assume(q,'real')
+syms d [1 x];
+assume(d,'real')
+syms a [1 x];
+assume(a,'real')
+%syms alpha [1 x];
+%assume(alpha,'real')
+alpha= zeros(x,1)';
+
+for i=1:x
+    if i==1
+        fprintf('Press y if parameter equals 0 else press n\n');
+    end
+    fprintf('Is q(%d)=',i);
+    yon=input('0? ','s');
+    if strcmp(yon,'y')==1
+        q(i)=0
+    end 
+end
+
+for i=1:x
+    if i==1
+        fprintf('Press y if parameter equals 0 else press n\n');
+    end
+    fprintf('Is d(%d)=',i);
+    yon=input('0? ','s');
+    if strcmp(yon,'y')==1
+        d(i)=0
+    end 
+end
+for i=1:x
+    if i==1
+        fprintf('Press y if parameter equals 0 else press n\n');
+    end
+    fprintf('Is a(%d)=',i);
+    yon=input('0? ','s');
+    if strcmp(yon,'y')==1
+        a(i)=0
+    end 
+end
+for i=1:x
+    if i==1
+        fprintf('Press y if parameter equals 0 else press n\n');
+    end
+    fprintf('Is alpha(%d) ',i);
+    yon=input('different? ','s');
+    if strcmp(yon,'y')==1
+        alpha(i)= input('Enter new alpha ');
+    end 
+end
+
+DH= [q; d; a; alpha]'
+
+ for i = 1:x    
+    T{i} = [cos(q(i)) -sin(q(i))*cos(alpha(i)) sin(q(i))*sin(alpha(i)) a(i)*cos(q(i));
+            sin(q(i)) cos(q(i))*cos(alpha(i)) -cos(q(i))*sin(alpha(i)) a(i)*sin(q(i));
+            0 sin(alpha(i)) cos(alpha(i)) d(i);        0 0 0 1];
+ end
+
+ T0i{1}=T{1};
+ for i=1:x-1
+     T0i{i+1}=T0i{i}*T{i+1};
+ end
+ 
+ p0=[0 0 0]';
+ z0=[0 0 1]'
+ for i=1:x
+     p(1:3,i)=T0i{i}(1:3,4);
+     z(1:3,i)=T0i{i}(1:3,3);
+ end
+ z(3,:)= round(z(3,:))
+syms Jp [3 x]
+syms Jo [3 x] 
+%  Jp=zeros(3,x);
+%  Jo=zeros(3,x);
+ for i=1:x
+     if por(i)=='p'
+         if i==1
+            Jp(1:3,i)= z0;
+            Jo(1:3,i)= [0 0 0]';
+         else
+            Jp(1:3,i)= z(:,i-1);
+            Jo(1:3,i)= [0 0 0]';
+         end
+         
+     elseif por(i)=='r'
+         if i==1
+            Jp(1:3,i)= cross(z0,p(:,x)-p0);
+            Jo(1:3,i)= z0;
+         else
+            Jp(1:3,i)= cross(z(:,i-1),p(:,x)-p(:,i-1));
+            Jo(1:3,i)= z(:,i-1);
+         end
+     end
+ end
+ J=[Jp;Jo];
+ J=simplify(J)
+%  Number_of_Links = x;
+%  dh = DH
+% for i = 1:Number_of_Links
+%  if por(i)=='r' % For all Revolute
+% for k = 1:Number_of_Links
+%     L{k} = Link('d',dh(k,2), 'a', dh(k,3), 'alpha', dh(k,4));
+% end
+% else % For all prismatic as they require some joint limits 
+%     disp(' As the joints you specified are prismatic you need to specify the limits of the joints - ')
+%     for m = 1:Number_of_Links
+%         limitlow(m)   = input(' Lower limit for the joint - ');
+%         limitupper(m) = input(' Upper limit for the joint - ');
+%         disp(' ')
+%     end
+%     for n = 1:Number_of_Links
+%         L{m}.qlim = [limitlow(m), limitupper(m)];
+%     end
+%     for k = 1:Number_of_Links
+%     L{k} = Link('theta',dh(k,1), 'a', dh(k,3), 'alpha', dh(k,4));
+%     end
+%  end
+%  end
+% % Making links using RVC Tools
+% % Link_array=[]
+% for b = 1:Number_of_Links
+%     % R = SerialLink([L{b}]);
+%     % Link_array=[Link_array,L{b}];
+%     X(b) = L{b};
+% end
+% n = 1:Number_of_Links;
+% m =[X(n)];
+% R = SerialLink(m);
+[m,n]=size(J);
+% %Ja=[];
+% m1=0;
+% n1=0;
+% 
+% %    syms z [n 1];
+%         z=zeros(1,n);
+% 
+% for i=1:m
+%     if J(i,:)==z
+%         %Ja(i,:)=J(i,:);
+%         m1=m1+1;
+%     end
+% end
+% 
+% for i=1:n
+%     if J(:,i)==z
+%         %Ja(:,i)=J(:,i);
+%         n1=n1+1;
+%     end
+% end
+% syms Ja [m-m1 n-n1]
+% %Ja=zeros(m-m1,n-n1);
+% %z1=zeros(1,n-n1);
+% 
+% %syms z1 [1 n];
+% z1=sym(zeros(1,n));
+% 
+% [f,g]=size(Ja);
+% j=1;
+% for i=1:m
+%     if isequal((J(i,:)),z1)
+%         
+%         %n1=n1+1;
+%     else
+%         fprintf('hi')
+%         Ja(j,:)=J(i,:);
+%         j=j+1;
+%     end
+% end 
+% %syms z1 [m 1];
+% j=1;
+% z=sym(zeros(m,1));
+% for i=1:n
+%     if isequal(J(:,i),z)
+%         
+%     else
+%         Ja(:,j)=J(:,i);
+%         fprintf('Hi');
+%         j=j+1;
+%         %m1=m1+1;
+%     end
+% end
+[m,n]=size(J);
+Ja=J;
+i=1;
+while i<m+1
+    if Ja(i,:)==zeros(1,n)
+    
+        Ja(i,:)=[];
+        %fprintf('%d',i);
+        i=i-1;
+        m=m-1;
+    end
+    i=i+1;
+end
+i=1;
+while i<n+1
+    if Ja(:,i)==zeros(m,1)
+    
+     Ja(:,i)=[];
+     n=n-1;
+     i=i-1;
+    end
+    i=i+1;
+end
